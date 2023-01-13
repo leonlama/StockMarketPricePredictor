041ee7a2f675a86a81c2c336966b6a47c73ae9b7
leonlama <118663690+leonlama@users.noreply.github.com>
2023-01-13 11:38:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-""""
+"""
 --- The Stock Price Predictor ---
 created by: León La Marca
+
+This programs aim is to predict the stock market price of a company using a simple neural network with 3 layers
+(in this program a sequential model is used). The neural network trains with the data provided by a finance api
+(e.g. Yahoo) and will then predict what the price of a stock will be on the followuing day.
+It is important that this is not any financial advise, I do not recommend trading based on the information
+you get from that program. It is for educational purpose only!
 """
 
 import numpy as np
@@ -8,6 +14,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pandas_datareader as web
 import datetime as dt
+import pandas_datareader as pdr
 
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.python.keras.models import Sequential
@@ -17,52 +24,53 @@ from tensorflow.python.keras.layers import Dense, Dropout, LSTM
 First the Programm should load the actual pricedata of a company using the yahoo finance page.
 """
 #Load The Data:
-company = 'FB'
+company = '\^AAPL' #\^ because we have to escape the \^
 
-start = dt.datetime(2012,1,1)
-end = dt.datetime(2022,1,1)
+#start = dt.datetime(2012,1,1)
+#end = dt.datetime(2022,1,1)
 
-data = web.DataReader(company, 'yahoo', start, end) #yahoo = finance API, man kann auch andere verwenden!
+data = pdr.DataReader(company, data_source='yahoo', start='2017-01-01', end='2020-09-28') #yahoo = finance API. You can use any finance API of your choice!
+#There is a Traceback error with the DataReader which i couldn´t fix :/
 
 """
 In order to work with the Data we loaded before, we have to modify and prepare the Data a little bit.
 This Program predicts the Stockprice for the next day, using the information of the past 60 days.
 """
 #Prepare the Data:
-scaler = MinMaxScaler(feature_range=(0,1)) #skaliert alle Werte (min Value und Max Value) zwischen 0-1
+scaler = MinMaxScaler(feature_range=(0,1)) #scales the values (min Value and Max Value) between 0-1
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1)) #Transform the Data (Closing Price!)
 
-prediciton_days = 60 #Auf wievielen Tagen soll die Prediction passieren
+prediciton_days = 60 #sets the amout days on which the prediction is based
 
 x_train = []
 y_train = []
 
-for x in range(prediciton_days, len(scaled_data)): #von 60 bis zum letzten Tag
-    x_train.append(scaled_data[x - prediciton_days:x]) #60 Tage
+for x in range(prediciton_days, len(scaled_data)): #from the 60th till the last day
+    x_train.append(scaled_data[x - prediciton_days:x]) #60 days
     y_train.append(scaled_data[x, 0])
 
-x_train = np.array(x_train) #In np Arrays convertieren
+x_train = np.array(x_train) #convert in np arrays
 y_train = np.array(y_train)
 
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1]), 1) #adds one dimension --> damit man dann damit im Neural Network (NN) arbeiten kann
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1]), 1) #adds one dimension --> so that the neural network (NN) is able to work with the data
 
 """
 Now we have to build the a Neural Network, which learns how to handle the data and then predicts the stock price
 for the next day. In this programm i use a Sequential model, which fits perfect for this example.
 """
 #Build the Model:
-model = Sequential() #model welches geeignete ist wenn man einzelne Values als input und output bei einem NN hat.
+model = Sequential() #that model is commonly used if you have single values as inputs and outputs in a NN.
 
-model.add(LSTM(units=50, return_sequences=True, input_shape = (x_train.shape[1], 1))) #Layers adden (LSTM = Long short-term Memory)
+model.add(LSTM(units=50, return_sequences=True, input_shape = (x_train.shape[1], 1))) #add layer (LSTM = Long short-term Memory)
 model.add(Dropout(0.2))
-model.add(LSTM(units=50), return_sequences=True) #Layers adden
+model.add(LSTM(units=50), return_sequences=True) #add layer
 model.add(Dropout(0.2))
-model.add(LSTM(units=50)) #Layers adden
+model.add(LSTM(units=50)) #add layer
 model.add(Dropout(0.2))
-model.add(Dense(units=1)) #Prediction of the next Closing Value
+model.add(Dense(units=1)) #outputs the prediction of the next closing value
 
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=25, batch_size=32) #epochs=25 --> model sieht die Daten 25 mal; batch_size = number of training models used in one iteration
+model.fit(x_train, y_train, epochs=25, batch_size=32) #epochs=25 --> model sees the data 25 times; batch_size = number of training models used in one iteration
 
 '''Test the Model accuracy on existing Data'''
 
@@ -77,9 +85,9 @@ test_end = dt.datetime.now()
 test_data = web.DataReader(company, 'yahoo', test_start, test_end)
 actual_prices = test_data['Close'].values #Close --> closing Values
 
-total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)  #verkettet die actual closing values mit den test closing values
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)  #concatenates the actual closing values with the test closing values
 
-model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediciton_days:].values #bestimmt womit das Model arbeitet um Predictions zu machen
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediciton_days:].values #determines the data that is used to predict the next closing value.
 model_inputs = model_inputs.reshape(-1, 1)
 model_inputs = scaler.transform(model_inputs) #scale it down
 
@@ -96,7 +104,7 @@ for x in range(prediciton_days, len(model_inputs)):
 x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1]), 1) #adds one dim
 
-predicted_prices = model.predict(x_test) # predicted prices --> scales --> rücktransformieren!
+predicted_prices = model.predict(x_test) # predicted prices --> scales --> retransform!
 predicted_prices = scaler.inverse_transform(predicted_prices)
 
 
@@ -125,4 +133,4 @@ real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1)) #
 
 prediction = model.predict(real_data)
 prediction = scaler.inverse_transform(prediction)
-print(f"Preditcion: {prediction}")
+print(f"Preditcion: {prediction}") #print the prediction into the console
